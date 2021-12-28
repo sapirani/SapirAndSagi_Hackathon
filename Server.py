05009867268e91555b935aa5c0cd907c042a43f2
sapirani <sapirani@post.bgu.ac.il>
2021-12-28 23:41:34 +0200

    adding exceptions in server

--- Server.py
+++ Server.py
@@ -55,20 +55,29 @@ def get_broadcast_message():
 
 # Send offer message over broadcast in UDP
 def offer_udp():
-    server_socket = socket(AF_INET, SOCK_DGRAM)
-    server_socket.bind((serverIP, serverPort))
+    try:
+        server_socket = socket(AF_INET, SOCK_DGRAM)
+        server_socket.bind((serverIP, serverPort))
+    except:
+        print(colored(255, 0, 0, "Can't connect to the given ip or port.")) # TODO: need break? is it good?
 
     while True:
-        if not is_in_game: # If the server allready has 2 clients connected and the game started, don't look for new clients
-            message = get_broadcast_message()
-            server_socket.sendto(message, UDP_client_address)
-        time.sleep(SECOND)
+        try:
+            if not is_in_game: # If the server allready has 2 clients connected and the game started, don't look for new clients
+                message = get_broadcast_message()
+                server_socket.sendto(message, UDP_client_address)
+            time.sleep(SECOND)
+        except:
+            continue
 
 # Get random question from the question bank (questions dictionary)
 def select_random_question():
-    max_len = len(question_bank)
-    question_number = random.randint(0,max_len - 1)
-    return  list(question_bank)[question_number]
+    try:
+        max_len = len(question_bank)
+        question_number = random.randint(0,max_len - 1)
+        return  list(question_bank)[question_number]
+    except:
+        return  list(question_bank)[0] # TODO: can we skip it?
 
 # Get the name of the winning team.
 # If the first client answerd first and correctly, he wins.
@@ -90,60 +99,69 @@ def get_winner_name(socket_with_answer, first_client_socket, second_client_socke
 
 # Starting the game after receiving two clients
 def handle_clients(first_client_socket, second_client_socket):
-    global is_in_game
-    is_in_game = True
-    first_player_name = first_client_socket.recv(MAX_BUF_SIZE).decode()
-    second_player_name = second_client_socket.recv(MAX_BUF_SIZE).decode()
+    try:
+        global is_in_game
+        is_in_game = True
+        first_player_name = first_client_socket.recv(MAX_BUF_SIZE).decode()
+        second_player_name = second_client_socket.recv(MAX_BUF_SIZE).decode()
 
-    # Select random question from the bank.
-    selected_question = select_random_question()
-    answer_to_the_question = question_bank.get(selected_question)
-    message = """
-    Quick Maths Is Starting!
-    Player 1: {first_player_name}
-    Player 2: {second_player_name}
-    ================================ 
-    Please answer the following question as fast as you can:
-    {question}""".format(first_player_name=first_player_name,
-                               second_player_name=second_player_name, question=selected_question)
+        # Select random question from the bank.
+        selected_question = select_random_question()
+        answer_to_the_question = question_bank.get(selected_question)
+        message = """
+        Quick Maths Is Starting!
+        Player 1: {first_player_name}
+        Player 2: {second_player_name}
+        ================================ 
+        Please answer the following question as fast as you can:
+        {question}""".format(first_player_name=first_player_name,
+                                second_player_name=second_player_name, question=selected_question)
 
-    first_client_socket.send(message.encode())
-    second_client_socket.send(message.encode())
+        first_client_socket.send(message.encode())
+        second_client_socket.send(message.encode())
 
-    # The selector knows which client answered first.
-    readable, _, _ = select.select([first_client_socket, second_client_socket], [], [], TIMEOUT)
+        # The selector knows which client answered first.
+        readable, _, _ = select.select([first_client_socket, second_client_socket], [], [], TIMEOUT)
 
-    game_message = """
-    Game over!
-    The correct answer was {answer}!""".format(answer=answer_to_the_question)
+        game_message = """
+        Game over!
+        The correct answer was {answer}!""".format(answer=answer_to_the_question)
 
-    if len(readable) == 0: # If non of the clients answered, it's a draw.
-        game_message += "\n    Game result: Draw\n"
+        if len(readable) == 0: # If non of the clients answered, it's a draw.
+            game_message += "\n    Game result: Draw\n"
 
-    else: # Else, there is a winner to the game.
-        winner = get_winner_name(readable[0], first_client_socket, second_client_socket,
-                                 first_player_name, second_player_name, answer_to_the_question)
-        game_message += "\n    Congratulations to the winner: {winner}\n".format(winner=winner)
+        else: # Else, there is a winner to the game.
+            winner = get_winner_name(readable[0], first_client_socket, second_client_socket,
+                                    first_player_name, second_player_name, answer_to_the_question)
+            game_message += "\n    Congratulations to the winner: {winner}\n".format(winner=winner)
 
-    first_client_socket.send(game_message.encode())
-    second_client_socket.send(game_message.encode())
+        first_client_socket.send(game_message.encode())
+        second_client_socket.send(game_message.encode())
 
-    is_in_game = False # The game ended, start sending offer messages again.
+        is_in_game = False # The game ended, start sending offer messages again.
+    except:
+        print(colored(255, 0, 0, "Something is wrong with the game... Game ended."))
 
 # Recieve two clients over TCP
 def receive_clients_tcp():
-    server_socket = socket(AF_INET, SOCK_STREAM)
-    server_socket.bind((serverIP, serverPort))
-    server_socket.listen(1)
-
+    try:
+        server_socket = socket(AF_INET, SOCK_STREAM)
+        server_socket.bind((serverIP, serverPort))
+        server_socket.listen(1)
+    except:
+        print(colored(255, 0, 0, "Can't connect to the given ip or port.")) # TODO: need break? is it good?
+    
     while True:
-        connection_socket_first_client, addr = server_socket.accept()
-        connection_socket_second_client, addr = server_socket.accept()
+        try:
+            connection_socket_first_client, addr = server_socket.accept()
+            connection_socket_second_client, addr = server_socket.accept()
 
-        handle_clients(connection_socket_first_client, connection_socket_second_client)
+            handle_clients(connection_socket_first_client, connection_socket_second_client)
 
-        connection_socket_first_client.close()
-        connection_socket_second_client.close()
+            connection_socket_first_client.close()
+            connection_socket_second_client.close()
+        except:
+            continue
 
 
 def main():
